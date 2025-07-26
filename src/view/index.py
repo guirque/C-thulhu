6e81a8c88b5d40dda28bf53ab9552509f62962a7
EvilFactory <evilfactory@users.noreply.github.com>
2025-07-25 23:31:45 -0300

    feat: chat history

--- src/view/index.py
+++ src/view/index.py
@@ -12,23 +12,38 @@ st.title("🦜🔗 Folder organizer AI")
 value = st.sidebar.text_input("Google API Key", type="password", value=os.getenv("GEMINI_API_KEY", ""))
 update_api_key(value)
 
+def initialize_session_state():
+    """Initialize the session state of Streamlit"""    
+    if "folder_path" not in st.session_state:
+        st.session_state.folder_path = None
+    if "messages" not in st.session_state:
+        st.session_state.messages = [{"role": "assistant", "content": "How can I help you to organize your folders?"}]
 
-with st.form("working_dir"):
-    submitted = st.form_submit_button("Choose AI Working Directory")
-    if submitted:
-        st.session_state['folder_path'] = easygui.diropenbox("Select a folder")
+initialize_session_state()
 
-with st.form("submit"):
-    text = st.text_area(
-        "Type the criteria that you want for reorganizing your files:",
-        "Documents that look have similar subjects",
-    )
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
 
-    submitted = st.form_submit_button("Run")
+if prompt := st.chat_input():
     if not api_key_exists():
-        st.warning("Please enter your Google API key!", icon="⚠")
-    if submitted and api_key_exists():
-        if not st.session_state['folder_path']:
-            st.error("Folder not selected")
-        else:
-            st.info(invoke(text, st.session_state['folder_path'])['messages'][-1].content)
+        st.info("Please add your Google Gemini key to continue.")
+        st.stop()
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+
+    if not st.session_state.folder_path:
+        st.error("Folder not selected")
+    else:
+        response = invoke(prompt, st.session_state['folder_path'])
+        msg = response['messages'][-1].content
+        st.session_state.messages.append({"role": "assistant", "content": msg})
+        st.chat_message("assistant").write(msg)
+
+with st.container():
+    st.markdown("### Working Directory")
+    if st.button("📁 Choose Folder"):
+        folder = easygui.diropenbox("Select a folder")
+        if folder:
+            st.session_state.folder_path = folder
+            st.success(f"Selected folder: {folder}")
